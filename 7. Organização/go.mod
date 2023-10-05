1ce6a3a8570f2037eb2c423f07dd17ca9d76c263
rafaelmm16 <rafael.merlomendes@gmail.com>
2023-10-05 15:03:40 -0300

    Organização e estruturação

---
+++ 7. Organização/go.mod
@@ -0,0 +1,3 @@
+module Consumer
+
+go 1.21.1
---
+++ 7. Organização/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"Consumer/pkg/web"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Request")
+
+	users, err := web.GetUsers()
+	if err != nil {
+		fmt.Println("Deu erro:", err)
+	}
+
+	for i, v := range users {
+		fmt.Printf("[%d] User: %s\n", i, v.Name)
+	}
+}
